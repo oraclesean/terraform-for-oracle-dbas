7f7eb436ea6477de05304d906628bc2655db7542
oraclesean <oracle.sean@gmail.com>
2024-08-23 03:19:46 +0000

    Update providers.tf for Resource Manager

--- providers.tf
+++ providers.tf
@@ -1,9 +1,10 @@
 provider "oci" {
   tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
   region           = var.region
+## Uncomment the following when running from the Terraform CLI:
+#  user_ocid        = var.user_ocid
+#  fingerprint      = var.fingerprint
+#  private_key_path = var.private_key_path
 }
 
 terraform {
